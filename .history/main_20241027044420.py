97902080017ae573f2a46a950686a1fbdfc6dc5e
civvic1 <victorcarracedo@gmail.com.com>
2024-10-27 04:46:23 +0100

    cambio de puerto al 8080

--- .history/main_20241027044420.py
+++ .history/main_20241027044420.py
@@ -65,5 +65,6 @@ def predecir():
     return render_template('resultado.html', resultado=resultado, periodo_dia=periodo_dia, amount=amount)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8080 main.py_object()
+            , debug=True)
 #prueba
